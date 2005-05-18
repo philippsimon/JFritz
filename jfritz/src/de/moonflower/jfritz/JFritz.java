67af4e349ee79ce39744d9ebab31ecaf787854b9
akw <>
2005-05-18 13:23:12 +0000

    added robert palmer

--- jfritz/src/de/moonflower/jfritz/JFritz.java
+++ jfritz/src/de/moonflower/jfritz/JFritz.java
@@ -18,9 +18,9 @@
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * This driver is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * This piece of software is distributed in the hope that it will be 
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
