96a5032b8af4ff1e16107d2605a9f4c7421529a3
akw <>
2005-05-29 22:03:31 +0000

    confirmOnExit

--- jfritz/src/de/moonflower/jfritz/JFritz.java
+++ jfritz/src/de/moonflower/jfritz/JFritz.java
@@ -21,7 +21,7 @@
  *
  * This piece of software is distributed in the hope that it will be 
  * useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * of MERCHANTABILITY or FöITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
