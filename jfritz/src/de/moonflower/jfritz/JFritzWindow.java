24e336e34e453796b43fe944bdb7f7e447d3b64a
robotniko <>
2005-07-15 16:37:29 +0000

    Lookup after Fetch

--- jfritz/src/de/moonflower/jfritz/JFritzWindow.java
+++ jfritz/src/de/moonflower/jfritz/JFritzWindow.java
@@ -471,6 +471,10 @@ public class JFritzWindow extends JFrame implements Runnable, ActionListener,
 					setStatus();
 					jfritz.getCallerlist().fireTableStructureChanged();
 					isretrieving = false;
+					if (JFritz.getProperty("option.lookupAfterFetch", "false")
+							.equals("true")) {
+						lookupButton.doClick();
+					}
 				}
 			};
 			worker.start();
@@ -627,6 +631,7 @@ public class JFritzWindow extends JFrame implements Runnable, ActionListener,
 			// FIXME if (jfritz.getTelnet() != null)
 			// jfritz.getTelnet().interrupt();
 			jfritz.stopSyslogListener();
+			jfritz.stopYACListener();
 			System.exit(0);
 		}
 	}
