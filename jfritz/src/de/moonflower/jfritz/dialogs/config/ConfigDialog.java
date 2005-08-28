a5ea924b1468bcc686934e522406866f08bd2360
robotniko <>
2005-08-28 16:01:47 +0000

    Config for call by call

--- jfritz/src/de/moonflower/jfritz/dialogs/config/ConfigDialog.java
+++ jfritz/src/de/moonflower/jfritz/dialogs/config/ConfigDialog.java
@@ -92,7 +92,7 @@ public class ConfigDialog extends JDialog {
 			notifyOnCallsButton, confirmOnExitButton, startMinimizedButton,
 			timerAfterStartButton, passwordAfterStartButton, soundButton,
 			callMonitorAfterStartButton, lookupAfterFetchButton,
-			syslogPassthroughCheckBox;
+			syslogPassthroughCheckBox, showCallByCallButton;
 
 	private JPanel callMonitorPane, yacMonitorPane, telnetMonitorPane,
 			syslogMonitorPane, callmessageMonitorPane;
@@ -190,7 +190,10 @@ public class ConfigDialog extends JDialog {
 
 		lookupAfterFetchButton.setSelected(JFritzUtils.parseBoolean(JFritz
 				.getProperty("option.lookupAfterFetch", "false")));
-
+		
+		showCallByCallButton.setSelected(JFritzUtils.parseBoolean(JFritz
+				.getProperty("option.showCallByCall", "false")));
+		
 		boolean pwAfterStart = !Encryption.decrypt(
 				JFritz.getProperty("jfritz.password", "")).equals(
 				JFritz.PROGRAM_SECRET
@@ -281,6 +284,9 @@ public class ConfigDialog extends JDialog {
 		JFritz.setProperty("option.lookupAfterFetch", Boolean
 				.toString(lookupAfterFetchButton.isSelected()));
 
+		JFritz.setProperty("option.showCallByCall", Boolean
+				.toString(showCallByCallButton.isSelected()));
+		
 		JFritz.setProperty("option.syslogclientip", ipAddressComboBox
 				.getSelectedItem().toString());
 
@@ -511,6 +517,11 @@ public class ConfigDialog extends JDialog {
 		lookupAfterFetchButton = new JCheckBox(
 				"Nach Laden Rückwärtssuche ausführen");
 		cPanel.add(lookupAfterFetchButton, c);
+
+		c.gridy = 4;
+		showCallByCallButton = new JCheckBox(
+				"Call-By-Call Informationen anzeigen");
+		cPanel.add(showCallByCallButton, c);
 
 		return cPanel;
 	}
