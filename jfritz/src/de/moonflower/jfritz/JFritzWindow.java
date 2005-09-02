01efea3c0c844a8a341b7f3e0ddb89f216caaa01
robotniko <>
2005-09-02 09:44:50 +0000

    Call-By-Call show/hide bugfix

--- jfritz/src/de/moonflower/jfritz/JFritzWindow.java
+++ jfritz/src/de/moonflower/jfritz/JFritzWindow.java
@@ -179,7 +179,7 @@ public class JFritzWindow extends JFrame implements Runnable, ActionListener,
 		callerListPanel = new CallerListPanel(jfritz);
 		phoneBookPanel = new PhoneBookPanel(jfritz);
 		quickDialPanel = new QuickDialPanel(jfritz);
-		
+
 		tabber = new JTabbedPane(JTabbedPane.BOTTOM);
 		tabber.addTab(JFritz.getMessage("callerlist"), callerListPanel);
 		tabber.addTab(JFritz.getMessage("phonebook"), phoneBookPanel);
@@ -620,14 +620,25 @@ public class JFritzWindow extends JFrame implements Runnable, ActionListener,
 			// Show / hide CallByCall column
 			if (JFritzUtils.parseBoolean(JFritz.getProperty(
 					"option.showCallByCall", "false"))) {
-				TableColumnModel colModel = jfritz.getJframe().getCallerTable().getColumnModel();
-				colModel.addColumn(jfritz.getJframe().getCallerTable().getCallByCallColumn());
-				colModel.moveColumn(colModel.getColumnCount()-1, 2);
-				colModel.getColumn(2).setPreferredWidth(Integer.parseInt(JFritz.getProperty(
-						"column2.width", "60")));
+				TableColumnModel colModel = jfritz.getJframe().getCallerTable()
+						.getColumnModel();
+				if (!colModel.getColumn(2).getHeaderValue().toString().equals(
+						"Call-By-Call")) {
+					colModel.addColumn(jfritz.getJframe().getCallerTable()
+							.getCallByCallColumn());
+					colModel.moveColumn(colModel.getColumnCount() - 1, 2);
+					colModel.getColumn(2).setPreferredWidth(
+							Integer.parseInt(JFritz.getProperty(
+									"column2.width", "60")));
+				}
 			} else {
-				TableColumnModel colModel = jfritz.getJframe().getCallerTable().getColumnModel();
-				colModel.removeColumn(colModel.getColumn(2));
+				TableColumnModel colModel = jfritz.getJframe().getCallerTable()
+						.getColumnModel();
+				if (colModel.getColumn(2).getHeaderValue().toString().equals(
+						"Call-By-Call")) {
+
+					colModel.removeColumn(colModel.getColumn(2));
+				}
 			}
 		}
 		configDialog.dispose();
