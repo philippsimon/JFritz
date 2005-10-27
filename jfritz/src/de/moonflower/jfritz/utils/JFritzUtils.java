0d752e77d65731fbbde48421b0739c4c9a9433ce
robotniko <>
2005-10-27 14:24:11 +0000

    Changed JavaDoc

--- jfritz/src/de/moonflower/jfritz/utils/JFritzUtils.java
+++ jfritz/src/de/moonflower/jfritz/utils/JFritzUtils.java
@@ -188,8 +188,6 @@ public class JFritzUtils {
 
     /**
      * 
-     * @param box_address
-     * @param box_password
      * @param firmware
      * @return Vector of QuickDial objects
      * @throws WrongPasswordException
