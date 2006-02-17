b2ffcb3b488914aa860364380b45b8eea6fcf1b2
kleinch <>
2006-02-17 13:55:09 +0000

    汨牥‬扯戠楥⁭敒敶獲ⵥ潌歯灵ﰠ敢桲畡瑰眠獡渠捡杨獥档畡⁴畷摲ⱥ眠敮⁮敮湩‬敔敬潦扮捵⁨楮档⁴灳楥档牥੮

--- jfritz/src/de/moonflower/jfritz/JFritzWindow.java
+++ jfritz/src/de/moonflower/jfritz/JFritzWindow.java
@@ -538,7 +538,8 @@ public class JFritzWindow extends JFrame implements Runnable, ActionListener,
             final SwingWorker worker = new SwingWorker() {
                 public Object construct() {
                     boolean isdone = false;
-                    while (!isdone) {
+					int j = 0;
+					while (!isdone) {
                         setBusy(true);
                         setStatus(JFritz.getMessage("reverse_lookup"));
                         for (int i = 0; i < jfritz.getCallerlist()
@@ -548,7 +549,8 @@ public class JFritzWindow extends JFrame implements Runnable, ActionListener,
                             Call call = (Call) data.get(i);
                             PhoneNumber number = call.getPhoneNumber();
                             if (number != null && (call.getPerson() == null)) {
-                                setStatus(JFritz
+                                j++;
+								setStatus(JFritz
                                         .getMessage("reverse_lookup_for")
                                         + " " + number.getIntNumber() + " ...");
                                 Debug.msg("Reverse lookup for "
@@ -565,10 +567,9 @@ public class JFritzWindow extends JFrame implements Runnable, ActionListener,
 
                             }
                         }
-
                         isdone = true;
                     }
-                    jfritz.getPhonebook().saveToXMLFile(JFritz.PHONEBOOK_FILE);
+                    if (j > 0) jfritz.getPhonebook().saveToXMLFile(JFritz.PHONEBOOK_FILE);
                     return null;
                 }
 
