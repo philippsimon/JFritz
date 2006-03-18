00ec72fe4a3374e260037ca160c46093af4ae1de
baefer <>
2006-03-18 10:01:42 +0000

    *** empty log message ***

--- jfritz/src/de/moonflower/jfritz/callerlist/CommentCellEditor.java
+++ jfritz/src/de/moonflower/jfritz/callerlist/CommentCellEditor.java
@@ -77,7 +77,7 @@ public class CommentCellEditor extends AbstractCellEditor implements
 		super.fireEditingCanceled();
 	}
 
-	/*
+	/**
 	 * @see javax.swing.AbstractCellEditor#fireEditingStopped()
 	 */
 	protected void fireEditingStopped() {
