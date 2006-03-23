a528fbbf91d96090d4bb143c23594062d9e2837a
baefer <>
2006-03-23 12:17:44 +0000

    Changelog

--- jfritz/src/de/moonflower/jfritz/JFritz.java
+++ jfritz/src/de/moonflower/jfritz/JFritz.java
@@ -42,6 +42,7 @@
  * TODO: Checken, ob alle Bibliotheken vorhanden sind
  * 
  * JFritz 0.5.6
+ * - Neu: Wahlhilfe
  * - Änderung: Das Durchsuchen der Anruferliste muss nun per [ENTER] gestartet werden.
  * - Bugfix: "Übernehmen" Button im Telefonbuch wird nun anklickbar, wenn man eine Telefonnummer geändert hat.
  * - Bugfix: Sonderzeichen bei "Externes Programm starten" werden korrekt gespeichert 
