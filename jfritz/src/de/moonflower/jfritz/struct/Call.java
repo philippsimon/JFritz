20de4aa7453014bc552cefc6d79547b87ecdb2dc
marc0815 <>
2006-10-21 13:43:41 +0000

    speedup sorting callerlist with persons

--- jfritz/src/de/moonflower/jfritz/struct/Call.java
+++ jfritz/src/de/moonflower/jfritz/struct/Call.java
@@ -39,6 +39,8 @@ public class Call {
 
     private double cost= -1;
     
+    private Person cachePerson;
+    
     private String comment = ""; //$NON-NLS-1$
 
     public Call(CallType calltype, Date calldate,
@@ -103,9 +105,11 @@ public class Call {
      * @return Returns the person the number belongs to or null.
      */
     public Person getPerson() {
+    	if(cachePerson!=null){return cachePerson;}
     	Person retValue = getPerson(false);
     	if (retValue==null)
     		retValue = getPerson(true);
+    	cachePerson = retValue;
     	return retValue;
     }
     
