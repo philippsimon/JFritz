605cc3f0b403211efb8fdc8a0921d21d680b899d
marc0815 <>
2006-10-21 13:50:33 +0000

    exit now generates a WindowEvent

--- jfritz/src/de/moonflower/jfritz/JFritzWindow.java
+++ jfritz/src/de/moonflower/jfritz/JFritzWindow.java
@@ -1020,7 +1020,9 @@ ItemListener {
 	public void actionPerformed(ActionEvent e) {
 		Debug.msg("Action " + e.getActionCommand()); //$NON-NLS-1$
 		if (e.getActionCommand().equals("exit")) {
-			showExitDialog();
+//			showExitDialog();
+			WindowEvent we = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
+			processWindowEvent(we);
 		} else if (e.getActionCommand().equals("about")) {
 			showAboutDialog();
 		} else if (e.getActionCommand().equals("help")) { //$NON-NLS-1$
