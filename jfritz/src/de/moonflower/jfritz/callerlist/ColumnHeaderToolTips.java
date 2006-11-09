1cfb788863b453cf875c6fa93e20cf0109ba08da
marc0815 <>
2006-11-09 01:53:38 +0000

    warning wegen templates entfernt und Thread in reverselookup eingebaut

--- jfritz/src/de/moonflower/jfritz/callerlist/ColumnHeaderToolTips.java
+++ jfritz/src/de/moonflower/jfritz/callerlist/ColumnHeaderToolTips.java
@@ -27,7 +27,7 @@ public class ColumnHeaderToolTips extends MouseMotionAdapter {
     TableColumn curCol;
 
     // Maps TableColumn objects to tooltips
-    Map tips = new HashMap();
+    Map<TableColumn, String> tips = new HashMap<TableColumn, String>();
 
     // If tooltip is null, removes any tooltip text.
     public void setToolTip(TableColumn col, String tooltip) {
@@ -51,7 +51,7 @@ public class ColumnHeaderToolTips extends MouseMotionAdapter {
         }
 
         if (col != curCol) {
-            header.setToolTipText((String)tips.get(col));
+            header.setToolTipText(tips.get(col));
             curCol = col;
         }
     }
