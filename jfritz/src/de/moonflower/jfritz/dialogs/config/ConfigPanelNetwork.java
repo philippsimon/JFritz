89369459d460f1b489f58df74c5215d0fedd0259
capncrunch <>
2007-10-17 09:33:14 +0000

    various bugfixes related to network code

--- jfritz/src/de/moonflower/jfritz/dialogs/config/ConfigPanelNetwork.java
+++ jfritz/src/de/moonflower/jfritz/dialogs/config/ConfigPanelNetwork.java
@@ -192,7 +192,6 @@ public class ConfigPanelNetwork extends JPanel implements ConfigPanel, ActionLis
 		}
 		
 		JFritz.getJframe().setNetworkButton();
-		NetworkStateMonitor.serverStateChanged();
 		ClientLoginsTableModel.saveToXMLFile(Main.SAVE_DIR + JFritz.CLIENT_SETTINGS_FILE);
 		
 	}
@@ -407,7 +406,7 @@ public class ConfigPanelNetwork extends JPanel implements ConfigPanel, ActionLis
 		panel.add(startClientButton, c);
 		
 		sPanel = new JScrollPane(panel);
-		
+	
 		return sPanel;
 	}
 
