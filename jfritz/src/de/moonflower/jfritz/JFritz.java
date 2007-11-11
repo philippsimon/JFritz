471c94d71d504fa1a75522ee7a24e62acbbea747
robotniko <>
2007-11-11 00:38:48 +0000

    - Bugfix: Einstellungsfenster wird zentriert - Bugfix: "Verbindungsgerät" in "MSN/Rufnummer" ändern - Bugfix: Anzeige von "Durchwahl" statt "3" in der Anrufliste - Neu: Unterstützung für Fritz!Box 7270, Fritz!Box 7140, Fritz!Box 7141, Fritz!Box 7113, Fritz!Box 5140 - Wiederherstellung des gespeicherten Fensterstatuses verändert

--- jfritz/src/de/moonflower/jfritz/JFritz.java
+++ jfritz/src/de/moonflower/jfritz/JFritz.java
@@ -5,6 +5,7 @@
 
 package de.moonflower.jfritz;
 
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -109,9 +110,6 @@ public final class JFritz implements  StatusListener{
 	private static Timer watchdogTimer;
 
 	private static FritzBox fritzBox;
-
-	private static int oldFrameState; // saves old frame state to restore old
-									  // state
 
 	private static QuickDials quickDials;
 	
@@ -511,14 +509,22 @@ public final class JFritz implements  StatusListener{
 
 	public static void hideShowJFritz() {
 		if (jframe.isVisible()) {
-			oldFrameState = jframe.getExtendedState();
 			Debug.msg("Hide JFritz-Window"); //$NON-NLS-1$
+			Debug.msg(Main.getStateProperty("window.property.old", 
+					Integer.toString(Frame.NORMAL)));
+			Main.setStateProperty("window.state.old", Main.getProperty("window.state", Integer.toString(Frame.NORMAL)));
+			Main.setStateProperty("window.state", Integer
+					.toString(Frame.ICONIFIED));
 			jframe.setExtendedState(JFrame.ICONIFIED);
-			jframe.setVisible(false);
-		} else {
+//			jframe.setVisible(false);
+		} else while ( !jframe.isVisible() ){
 			Debug.msg("Show JFritz-Window"); //$NON-NLS-1$
+			Debug.msg(Main.getStateProperty("window.property.old", 
+					Integer.toString(Frame.NORMAL)));
 			jframe.setVisible(true);
-			jframe.setExtendedState(oldFrameState);
+			jframe.setExtendedState(Integer.parseInt(
+					Main.getStateProperty("window.property.old", 
+							Integer.toString(Frame.NORMAL))));
 			jframe.toFront();
 			jframe.repaint();
 		}
