a7343760861072f55671e384962317da356c2888
robotniko <>
2008-09-18 17:22:29 +0000

    JFritz 0.7.1.13 - Bugfix: Korrektes Abholen der Anrufliste von der 54.04.63-12190 labor firmware. - Neu: Wizard wird nun zentriert dargestellt. - Bugfix: Falls MAC-Adresse nicht bestimmt werden kann, keine Warnung bei vermeintlich neuer Box mehr ausgeben. - Bugfix: Korrektes speichern des Passworts in den Einstellungen auch ohne Klick auf "Box erkennen" - Bugfix: Autodetection bei Auswahl der FritzBox in den Einstellungen deaktiviert, weil in die labor-fw eine Sperre wegen falschem Passwort eingebaut wurde.

--- jfritz/src/de/moonflower/jfritz/utils/JFritzUtils.java
+++ jfritz/src/de/moonflower/jfritz/utils/JFritzUtils.java
@@ -11,6 +11,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Calendar;
@@ -77,10 +78,15 @@ public class JFritzUtils {
 			if (postdata != null) {
 				urlConn.setRequestProperty("Content-Type", //$NON-NLS-1$
 						"application/x-www-form-urlencoded"); //$NON-NLS-1$
-				printout = new DataOutputStream(urlConn.getOutputStream());
-				printout.writeBytes(postdata);
-				printout.flush();
-				printout.close();
+				try {
+					printout = new DataOutputStream(urlConn.getOutputStream());
+					printout.writeBytes(postdata);
+					printout.flush();
+					printout.close();
+				} catch (SocketTimeoutException ste)
+				{
+					Debug.err("Could not fetch data from url: "+ste.toString());
+				}
 			}
 
 			BufferedReader d;
