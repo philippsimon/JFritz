943e84c610ede2645abebf1066a32f629d287143
robotniko <>
2009-02-14 13:43:25 +0000

    䙊楲穴〠㜮㈮ㄮ㨴ⴊ丠略›敒獳畯捲湥椠⁮楥敧敮⁳慪⁲敧慰正⹴丠湵欠滶敮⁮楤獥⁥敲慬楴⁶敬捩瑨愠獵敧慴獵档⁴敷摲湥ਮ

--- jfritz/src/de/moonflower/jfritz/Main.java
+++ jfritz/src/de/moonflower/jfritz/Main.java
@@ -200,7 +200,7 @@ public class Main implements LookupObserver {
 
 	public final static String PROGRAM_NAME = "JFritz"; //$NON-NLS-1$
 
-	public final static String PROGRAM_VERSION = "0.7.2.13"; //$NON-NLS-1$
+	public final static String PROGRAM_VERSION = "0.7.2.14"; //$NON-NLS-1$
 	
 	public final static String CVS_TAG = "$Id$"; //$NON-NLS-1$
 
