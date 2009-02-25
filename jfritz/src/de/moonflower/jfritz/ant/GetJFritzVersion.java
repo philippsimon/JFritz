473a07fa0f37dd56163b52add93b7903e11477a4
robotniko <>
2009-02-25 19:59:02 +0000

    Added some new ANT-Tasks to set correct JFritz-Version in Build.xml

---
+++ jfritz/src/de/moonflower/jfritz/ant/GetJFritzVersion.java
@@ -0,0 +1,1 @@
+package de.moonflower.jfritz.ant;import org.apache.tools.ant.Project;import de.moonflower.jfritz.Main;public class GetJFritzVersion {	Project project;    	public void setProject(Project proj) {        project = proj;    }	public void execute() {		project.setProperty("version", Main.PROGRAM_VERSION);	}}
