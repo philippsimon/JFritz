3251fdf28ad2b6ab11db485eb3b21ee7aa76f53a
robotniko <>
2009-03-24 17:51:40 +0000

    JFritz 0.7.2.30: - Debug-View an umgeschriebenen Logger angepasst. Strings: refresh

--- jfritz/src/de/moonflower/jfritz/Main.java
+++ jfritz/src/de/moonflower/jfritz/Main.java
@@ -200,7 +200,7 @@ public class Main implements LookupObserver {
 
 	public final static String PROGRAM_NAME = "JFritz"; //$NON-NLS-1$
 
-	public final static String PROGRAM_VERSION = "0.7.2.29"; //$NON-NLS-1$
+	public final static String PROGRAM_VERSION = "0.7.2.30"; //$NON-NLS-1$
 	
 	public final static String CVS_TAG = "$Id$"; //$NON-NLS-1$
 
